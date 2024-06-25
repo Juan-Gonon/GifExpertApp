83b7aa363a2c6e87f1fe34e919752de5f0f95f61
Juan Gonon <juanfegonon27@gmail.com>
2024-06-25 10:12:45 -0600

    GiftItem

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,30 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getGif } from '../helpers/getGifs'
+import { GifItem } from './GifItem'
 
 // eslint-disable-next-line react/prop-types
 export function GifGrid ({ category }) {
+  const [image, setImage] = useState([])
+
   useEffect(() => {
-    getGif({ category })
+    const newImage = async () => {
+      const newImg = await getGif(category)
+      setImage(newImg)
+    }
+
+    newImage()
   }, [category])
 
   return (
     <>
       <h4>{category}</h4>
-      <p>Hola Mundo</p>
+      <div className='card-grid'>
+        {
+          image?.map((img) => (
+            <GifItem key={img.id} {...img} />
+          ))
+        }
+      </div>
     </>
   )
 }
