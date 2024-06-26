9c5da4dc47244ee949f82260a1a6904177815c9c
Juan Gonon <juanfegonon27@gmail.com>
2024-06-26 15:32:11 -0600

    mock custom hook

--- test/components/GifGrid.test.jsx
+++ test/components/GifGrid.test.jsx
@@ -1,14 +1,52 @@
-import { describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import { GifGrid } from '../../src/components/GifGrid'
-import { render, screen } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
+import { useFetchGifs } from '../../src/hook/useFetchGifs'
+
+vi.mock('../../src/hook/useFetchGifs')
 
 describe('Pruebas en <GifGrid />', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
   const category = 'One Punch'
+
   it('debe de mostrar el loading inicialmente', () => {
+    useFetchGifs.mockReturnValue({
+      image: [],
+      isLoading: true
+    })
+
     render(<GifGrid category={category} />)
     screen.debug()
 
     expect(screen.getByText('Cargando...'))
     expect(screen.getByText(category))
+  })
+
+  it('Debe de mostrar items cuando se cargan las imágenes useFetchGif', () => {
+    const gifts = [
+      {
+        id: 'ABC',
+        title: 'vegueta',
+        url: 'https://localhost/vegueta.jpg'
+      },
+      {
+        id: '123',
+        title: 'Goku',
+        url: 'https://localhost/goku.jpg'
+      }
+    ]
+
+    useFetchGifs.mockReturnValue({
+      image: gifts,
+      isLoading: false
+    })
+
+    render(<GifGrid category={category} />)
+
+    screen.debug()
+    expect(screen.getAllByRole('img').length).toBe(2)
   })
 })
